0ec363c3f9d159bd320213919ff31adc339c9ec7
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-03-22 13:38:23 +0000

    Removed elb

--- ecs-case-notes/output.tf
+++ ecs-case-notes/output.tf
@@ -60,36 +60,6 @@ output "task_definition_revision" {
   value = "${module.app_task_definition.task_definition_revision}"
 }
 
-# ELB
-output "asg_elb_id" {
-  description = "The name of the ELB"
-  value       = "${module.create_app_elb.environment_elb_id}"
-}
-
-output "asg_elb_name" {
-  description = "The name of the ELB"
-  value       = "${module.create_app_elb.environment_elb_name}"
-}
-
-output "asg_elb_dns_name" {
-  description = "The DNS name of the ELB"
-  value       = "${module.create_app_elb.environment_elb_dns_name}"
-}
-
-output "asg_elb_source_security_group_id" {
-  description = "The ID of the security group that you can use as part of your inbound rules for your load balancer's back-end application instances"
-  value       = "${module.create_app_elb.environment_elb_source_security_group_id}"
-}
-
-output "asg_elb_zone_id" {
-  description = "The canonical hosted zone ID of the ELB (to be used in a Route 53 Alias record)"
-  value       = "${module.create_app_elb.environment_elb_zone_id}"
-}
-
-output "asg_elb_dns_cname" {
-  value = "${aws_route53_record.dns_entry.fqdn}"
-}
-
 # ECS Service
 output "ecs_service_id" {
   value = "${module.app_service.ecs_service_id}"
